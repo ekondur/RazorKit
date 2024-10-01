5076a876c4e8d4533da7c2ff69dcb9f9aeff6d1e
Emrah Kondur <emrah.kondur@invicti.com>
2024-10-01 23:26:56 +0300

    Added calbacks builder

--- src/Chart/Models/Options/Tooltip/Callbacks.cs
+++ src/Chart/Models/Options/Tooltip/Callbacks.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelperJS.Chart.Converters;
+using Newtonsoft.Json;
 
 namespace HelperJS.Chart.Models
 {
     public class Callbacks
     {
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string BeforeTitle { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string Title { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string AfterTitle { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string BeforeBody { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string BeforeLabel { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string Label { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string LabelColor { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string LabelTextColor { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string LabelPointStyle { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string AfterLabel { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string AfterBody { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string BeforeFooter { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string Footer { get; set; }
+
+        [JsonConverter(typeof(NoQuotesStringConverter))]
+        public string AfterFooter { get; set; }
     }
 }
