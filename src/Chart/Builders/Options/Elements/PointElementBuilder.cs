6be56a1f43c4aa2995c6b4061ade6eabef7aedcf
Emrah Kondur <emrah.kondur@invicti.com>
2024-10-20 22:26:56 +0300

    Added Point Element Builder

--- src/Chart/Builders/Options/Elements/PointElementBuilder.cs
+++ src/Chart/Builders/Options/Elements/PointElementBuilder.cs
@@ -25,5 +25,60 @@ namespace HelperJS.Chart.Builders
             _pointElement.Radius = radius;
             return this;
         }
+
+        /// <summary>
+        /// Point style.
+        /// </summary>
+        /// <param name="style"></param>
+        /// <returns></returns>
+        public PointElementBuilder PointStyle(PointStyle style)
+        {
+            _pointElement.PointStyle = style;
+            return this;
+        }
+
+        /// <summary>
+        /// Point rotation (in degrees). Default 0
+        /// </summary>
+        /// <param name="rotation"></param>
+        /// <returns></returns>
+        public PointElementBuilder Rotation(int rotation)
+        {
+            _pointElement.Rotation = rotation;
+            return this;
+        }
+
+        /// <summary>
+        /// Extra radius added to point radius for hit detection. Default 1
+        /// </summary>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public PointElementBuilder HitRadius(int radius)
+        {
+            _pointElement.HitRadius = radius;
+            return this;
+        }
+
+        /// <summary>
+        /// Point radius when hovered.. Default 4
+        /// </summary>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public PointElementBuilder HoverRadius(int radius)
+        {
+            _pointElement.HoverRadius = radius;
+            return this;
+        }
+
+        /// <summary>
+        /// Stroke width when hovered. Default 1
+        /// </summary>
+        /// <param name="width"></param>
+        /// <returns></returns>
+        public PointElementBuilder HoverBorderWidth(int width)
+        {
+            _pointElement.HoverBorderWidth = width;
+            return this;
+        }
     }
 }
